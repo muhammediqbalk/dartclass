facb950f0ef22f99e637dc22ebf20e9c9b0b92eb
muhammediqbalk <muhammediqbalk52@gmail.com>
2023-06-03 18:11:19 +0530

    commit

---
+++ bin/maths_operation.dart
@@ -0,0 +1,33 @@
+void add (){
+  int a = 10, b = 20;
+  print("sum is : ${a+b}");
+}
+
+void sub(){
+  int a = 20, b =51;
+  print("diff : ${a-b}");
+}
+ void multi(){
+  int a = 45, b = 65;
+  print("out :${a+b}");
+ }
+
+void div(){
+  int a = 95, b = 10;
+  print("out :${a/b}");
+}
+
+void main(){
+  ///since the function are inside the class no need for creating object
+  add();
+  sub();
+  multi();
+  div();
+}
+
+/// functions set of statement to perform particular task or operation
+ /* main categories of function
+    1. built in function    eg : main function
+    2. user defined function   eg : add() sub() etc.....
+
+ */
